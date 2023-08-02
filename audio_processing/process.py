8b262609d4fdfe7f65895ed07ff59ecaf8ef56fa
Daniel Lee <daniel.lee@circleci.com>
2023-08-01 21:20:29 -0700

    add new command

---
+++ audio_processing/process.py
@@ -0,0 +1,38 @@
+from pydub import AudioSegment
+
+import typer
+import random
+
+app = typer.Typer()
+
+
+BASE_PATH = '/Users/dannylee/Downloads/'
+
+
+@app.command()
+def insert_audio(audio: str, ad: str, start_time: int):
+    main_audio = AudioSegment.from_file(BASE_PATH + audio)
+    insert_ad = AudioSegment.from_file(BASE_PATH + ad)
+
+    start_time = start_time * 1000
+
+    first_part = main_audio[:start_time]
+    second_part = main_audio[start_time:]
+    new_audio = first_part + insert_ad + second_part
+
+    new_audio.export(BASE_PATH + f"insert_audio_{random.randint(100,999)}.wav", format="wav")
+
+
+@app.command()
+def replace_audio(audio: str, ad: str, start_time: int, end_time: int):
+    main_audio = AudioSegment.from_file(BASE_PATH + audio)
+    ad = AudioSegment.from_file(BASE_PATH + ad)
+
+    start_time = start_time * 1000
+    end_time = end_time * 1000
+
+    first_part = main_audio[:start_time]
+    second_part = main_audio[end_time:]
+    new_audio = first_part + ad + second_part
+
+    new_audio.export(BASE_PATH + f"replace_audio_{random.randint(100,999)}.wav", format="wav")

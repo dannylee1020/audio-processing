aa8fb0fc047411574e4f62709c8230896bddcd05
Daniel Lee <daniel.lee@circleci.com>
2023-08-06 19:49:01 -0700

    logic for processing audio files

--- audio_processing/process.py
+++ audio_processing/process.py
@@ -2,17 +2,16 @@ from pydub import AudioSegment
 
 import typer
 import random
+import io
 
 app = typer.Typer()
 
-
 BASE_PATH = '/Users/dannylee/Downloads/'
 
-
 @app.command()
-def insert_audio(audio: str, ad: str, start_time: int):
-    main_audio = AudioSegment.from_file(BASE_PATH + audio)
-    insert_ad = AudioSegment.from_file(BASE_PATH + ad)
+def insert_audio(audio_path: str, ad_path: str, start_time: int):
+    main_audio = AudioSegment.from_file(audio_path)
+    insert_ad = AudioSegment.from_file(ad_path)
 
     start_time = start_time * 1000
 
@@ -21,18 +20,18 @@ def insert_audio(audio: str, ad: str, start_time: int):
     new_audio = first_part + insert_ad + second_part
 
     new_audio.export(BASE_PATH + f"insert_audio_{random.randint(100,999)}.wav", format="wav")
-
+    return new_audio
 
 @app.command()
-def replace_audio(audio: str, ad: str, start_time: int, end_time: int):
-    main_audio = AudioSegment.from_file(BASE_PATH + audio)
-    ad = AudioSegment.from_file(BASE_PATH + ad)
+def replace_audio(audio_path: str, ad_path: str, start_time: int, end_time: int):
+    main_audio = AudioSegment.from_file(audio_path)
+    insert_ad = AudioSegment.from_file(ad_path)
 
     start_time = start_time * 1000
     end_time = end_time * 1000
 
     first_part = main_audio[:start_time]
     second_part = main_audio[end_time:]
-    new_audio = first_part + ad + second_part
+    new_audio = first_part + insert_ad + second_part
 
     new_audio.export(BASE_PATH + f"replace_audio_{random.randint(100,999)}.wav", format="wav")
